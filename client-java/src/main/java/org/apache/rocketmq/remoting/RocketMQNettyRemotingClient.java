92dad25b56660530fbeb3c319d7d77c9173b669b
mpif <caiszf@163.com>
2018-11-13 18:06:37 +0800

    Netty服务端和客户端测试

---
+++ client-java/src/main/java/org/apache/rocketmq/remoting/RocketMQNettyRemotingClient.java
@@ -0,0 +1,8 @@
+package org.apache.rocketmq.remoting;
+
+/**
+ * @author: ShengzhiCai
+ * @date: 2018-11-13 17:50
+ */
+public class RocketMQNettyRemotingClient {
+}
---
+++ client-java/src/main/java/org/apache/rocketmq/remoting/RocketMQNettyRemotingServer.java
@@ -0,0 +1,8 @@
+package org.apache.rocketmq.remoting;
+
+/**
+ * @author: ShengzhiCai
+ * @date: 2018-11-13 17:51
+ */
+public class RocketMQNettyRemotingServer {
+}
